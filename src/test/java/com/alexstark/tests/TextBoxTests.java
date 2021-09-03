57a987b7a150e9ca166e919acd97a5d425a36aba
Alexander Derevyanko <70107243+alexstark-spb@users.noreply.github.com>
2021-09-03 22:32:32 +0300

    added Text Box Tests

--- src/test/java/com/alexstark/tests/TextBoxTests.java
+++ src/test/java/com/alexstark/tests/TextBoxTests.java
@@ -1,10 +1,31 @@
 package com.alexstark.tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
     @Test
     void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Alex");
+        $("#userEmail").setValue("alexstark@mail.ru");
+        $("#currentAddress").setValue("S.Petersburg");
+        $("#permanentAddress").setValue("Nevskiy40");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alexstark@mail.ru"));
+        $("#output #currentAddress").shouldHave(text("S.Petersburg"));
+        $("#output #permanentAddress").shouldHave(text("yuj"));
 
 
     }
